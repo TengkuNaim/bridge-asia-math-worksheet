5b606ba0cc7532b89412c31980dc81f5e514855d
naimnoh <amirul@talentcloud.ai>
2025-04-14 00:19:01 +0800

    [MASTER]: github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bridge-asia-math-worksheet/',
   plugins: [
     vue(),
     vueJsx(),
