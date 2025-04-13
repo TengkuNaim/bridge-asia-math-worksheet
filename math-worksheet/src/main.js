d3ff3258ff472d2c4af7409370a10b91aadeac4b
naimnoh <amirul@talentcloud.ai>
2025-04-13 21:20:30 +0800

    [BETA]: 1. Add Worksheet Component to display the form. 2. Edit App.vue to use Worksheet

--- math-worksheet/src/main.js
+++ math-worksheet/src/main.js
@@ -1,10 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './assets/styles.css'
-
 
 const app = createApp(App)
 
